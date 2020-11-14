eb1d455459e65a16fb578a3712e3b8a9be8d2021
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-14 21:14:05 +0700

    Base Login and Add Bootstrap

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import './Login.css'
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="Login">
+                <div className="auth-inner">
+                    <h2 style={{textAlign:'center'}}>Login</h2>
+                    <Form>
+                        <Form.Group controId="formGroupEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Email" id="email"/>
+                        </Form.Group>
+                        <Form.Group controId="formGroupPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter Password" id="password"/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="btn btn-primary btn-block">Login</Button>
+                    </Form>
+                </div>
             </div>
         )
     }

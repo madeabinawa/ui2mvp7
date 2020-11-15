199ab42e195c58ba2fc59b27b58cd03429374d7e
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-15 19:33:30 +0700

    Add Logo in NavbarBPBA

--- src/navbar/NavbarBPBA.jsx
+++ src/navbar/NavbarBPBA.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import LogoBRI from '../Assets/BANK_BRI_logo.svg'
+import Users from '../Assets/user.svg'
 
 export default class NavbarBPBA extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="/">Litle Store</Navbar.Brand>
+                <Navbar bg="light" variant="light">
+                    <Navbar.Brand href="/">
+                    <img 
+                        alt=""
+                        src={LogoBRI}
+                        width="150"
+                        className="d-inline-block align-top"/>
+                    </Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="/DataUser">DATA USER</Nav.Link>
                         <Nav.Link href="/SetPBAM">PBAM</Nav.Link>

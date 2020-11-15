c073f56fd03a2af084adfa715c5ee28e87ac0a78
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-15 20:12:55 +0700

    Add Users Page to BPBA

---
+++ src/pages_BPBA/usersPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class usersPage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h3>DATA USERS</h3>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}

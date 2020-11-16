a15cde3f086301fea2622793466d27dce2ff79aa
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-16 07:55:40 +0700

    Add Modal in UserBPBA and change class to function

--- src/pages_BPBA/usersPage.jsx
+++ src/pages_BPBA/usersPage.jsx
@@ -1,28 +1,60 @@
-import React, { Component } from 'react'
-import {Form, Col, Row, Button } from 'react-bootstrap'
+import React,{ useState } from 'react'
+import { Form, Col, Row, Button, Modal } from 'react-bootstrap'
 
-export default class usersPage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="jumbotron jumbotron-fluid">
-                    <div className="container">
-                        <h3>DATA USERS</h3>
-                    </div>
+function AddUsersModal(props) {
+    return (
+        <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <h4>Centered Modal</h4>
+            <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+            </p>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default function UsersPage() {
+    const[modalAdd, setModalAdd] = useState(false);
+    return (
+        <React.Fragment>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h2 style={{fontWeight:'bold'}}>DATA USERS</h2>
                 </div>
-                <div>
-                    <Form>
-                        <Form.Group as={Row} controlId="formSearch">
-                            <Form.Label column sm="1">Cari: </Form.Label>
-                            <Col sm="5">
-                                <Form.Control type="text" placeholder="Role/Nama/Email" />
-                            </Col>
-                        </Form.Group>
-                        <h3>Daftar Users</h3>
-                        <Button variant="success" type="submit" className="float-right">TAMBAH USER</Button>
-                    </Form>
-                </div>
-            </React.Fragment>
-        )
-    }
+            </div>
+            <div className="container">
+                <Form>
+                    <Form.Group as={Row}>
+                    <Form.Label column sm="1">Cari: </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="text" placeholder="Role/Nama/Email"/>
+                    </Col>
+                    </Form.Group>
+                </Form>
+                <h3>Daftar Users</h3>
+                <Button variant="success" type="submit" className="float-right" onClick={() => setModalAdd(true)}>TAMBAH USER</Button>
+                
+                <AddUsersModal 
+                    show={modalAdd}
+                    onHide={() => setModalAdd(false)}
+                />
+            </div>
+        </React.Fragment>
+    )
 }

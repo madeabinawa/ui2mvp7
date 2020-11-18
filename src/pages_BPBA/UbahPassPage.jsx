a083281688487254410f79de84164da155d342da
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-18 07:30:44 +0700

    Done Ganti Password

--- src/pages_BPBA/UbahPassPage.jsx
+++ src/pages_BPBA/UbahPassPage.jsx
@@ -13,24 +13,29 @@ export default function UbahPassPage() {
                 <Form>
                 <Form.Group as={Row} controlId="formGroupName">
                     <Form.Label column sm="2">Password: </Form.Label>
-                    <Col sm="10">
+                    <Col sm="5">
                         <Form.Control type="password"/>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} controlId="formGroupName">
                     <Form.Label column sm="2">Password Baru: </Form.Label>
-                    <Col sm="10">
+                    <Col sm="5">
                         <Form.Control type="password"/>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} controlId="formGroupName">
                     <Form.Label column sm="2">Konfirmasi Password: </Form.Label>
-                    <Col sm="10">
+                    <Col sm="5">
                         <Form.Control type="password"/>
                     </Col>
                 </Form.Group>
                 </Form>
-                <Button variant="success" >Simpan</Button>
+                <Row>
+                    <Col sm="2" />
+                    <Col sm="5">
+                        <Button variant="success" className="btn btn-primary btn-block">Simpan</Button>
+                    </Col>
+                </Row>
             </div>
         </React.Fragment>
     )

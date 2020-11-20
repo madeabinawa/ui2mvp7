038b45c2b817465b6ddb8da12c2838b75c749694
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-20 14:19:27 +0700

    Add Users in Modal but can't hide the modal

--- src/pages_BPBA/UsersPage.jsx
+++ src/pages_BPBA/UsersPage.jsx
@@ -1,73 +1,236 @@
-import React,{ useState } from 'react'
+import React,{ useState, Component } from 'react'
 import { Form, Col, Row, Button, Modal, Table, ButtonGroup } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import NavBar from '../navbar/NavbarBPBA';
 import ShowAllUsers from './ApiListUser';
 
-function AddUsersModal(props) {
-    return (
-        <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        >
-        <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-            Tambah User
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form>
-                <Form.Group as={Row} controlId="formGroupRole">
-                    <Form.Label column sm="2">Role: </Form.Label>
-                    <Col sm="10">
-                        <Form.Control as="select" defaultValue="Choose...">
-                            <option>Priority Customers (PCu)</option>
-                            <option>Personal Banking Assistent (PBA)</option>
-                            <option>Personal Banking Assistent Manager (PBAM) </option>
-                            <option>Banking Premium Booking Administration (BPBA) </option>
-                        </Form.Control>
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formGroupName">
-                    <Form.Label column sm="2">Nama: </Form.Label>
-                    <Col sm="10">
-                        <Form.Control type="text"/>
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formGroupEmail">
-                    <Form.Label column sm="2">Email 1: </Form.Label>
-                    <Col sm="10">
-                        <Form.Control type="email"/>
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formGroupEmail2">
-                    <Form.Label column sm="2">Email 2: </Form.Label>
-                    <Col sm="10">
-                        <Form.Control type="email"/>
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="formGroupStatus">
-                    <Form.Label column sm="2">Status: </Form.Label>
-                    <Col sm="3">
-                        <Form.Control as="select" defaultValue="Choose...">
-                            <option>Enable</option>
-                            <option>Disable</option>
-                        </Form.Control>
-                    </Col>
-                </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant="success">Simpan</Button>
-            <Button variant="danger" onClick={props.onHide}>Batal</Button>
-        </Modal.Footer>
-        </Modal>
-    )
+const options = [
+    {
+        label: "ENABLE",
+        value: 1,
+    },
+    {
+        label: "DISABLE",
+        value: 0,
+    },
+];
+
+const selectrole = [
+    { 
+        label: "Priority Customers (PCu)",
+        value: 1,
+    },
+    {
+        label: "Personal Banking Assistent (PBA)",
+        value: 2,
+    },
+    {
+        label: "Personal Banking Assistent Manager (PBAM)",
+        value: 3,
+    },
+    {
+        label: "Banking Premium Booking Administration (BPBA)",
+        value: 4,
+    },
+];
+
+class AddUsersModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            nama: '',
+            alamat: '',
+            role_id: null,
+            email1: '',
+            email2: '',
+            phone1: '',
+            phone2: '',
+            password:'123456',
+            status: null,
+            onHide: false,
+            show: false,
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+       // const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.type === 'select' ? target.selected : target.value;
+        const name = target.name;
+    
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (event) => {
+        alert('data telah di tambahkan: ' + this.state.value);
+        fetch('http://18.191.9.5:8090/user/create', {
+            method: 'POST',
+            body: JSON.stringify({
+                "nama": this.state.nama,
+                "alamat": this.state.alamat,
+                "email1": this.state.email1,
+                "email2": this.state.email2,
+                "phone1": this.state.phone1,
+                "phone2": this.state.phone2,
+                "password": this.state.password,
+                "role_id": parseFloat(this.state.role_id),
+                "status": parseFloat(this.state.status)
+            })
+        }).then(function(response) {
+            console.log(response)
+            return response.json();
+        });
+        event.preventDefault();
+
+    }
+    render() {
+        return (
+            <Modal show={this.props.show} onHide={this.props.onHide}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                    Tambah User
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group as={Row} controlId="formGroupRole">
+                            <Form.Label column sm="2">Role: </Form.Label>
+                            <Col sm="10">
+                                <select className="form-control" name="role_id" value={this.state.role_id} onChange={this.handleInputChange}>
+                                    {selectrole.map((option) => (
+                                    <option value={option.value}>{option.label}</option>
+                                    ))}
+                                </select>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupName">
+                            <Form.Label column sm="2">Nama: </Form.Label>
+                            <Col sm="10">
+                                <Form.Control name="nama" type="text" value={this.state.nama} onChange={this.handleInputChange}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupAddress">
+                            <Form.Label column sm="2">Alamat: </Form.Label>
+                            <Col sm="10">
+                                <Form.Control name="alamat" type="text" value={this.state.alamat} onChange={this.handleInputChange} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupNumberHP">
+                            <Form.Label column sm="2">No. Handphone </Form.Label>
+                            <Col sm="10">
+                                <Form.Control name="phone1" type="text" value={this.state.phone1} onChange={this.handleInputChange} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupEmail">
+                            <Form.Label column sm="2">Email 1: </Form.Label>
+                            <Col sm="10">
+                                <Form.Control name="email1" type="email" value={this.state.email1} onChange={this.handleInputChange}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupEmail2">
+                            <Form.Label column sm="2">Email 2: </Form.Label>
+                            <Col sm="10">
+                                <Form.Control name="email2" type="email" value={this.state.email2} onChange={this.handleInputChange}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupStatus">
+                            <Form.Label column sm="2">Status: </Form.Label>
+                            <Col sm="3">
+                            <select className="form-control" name="status" value={this.state.status} onChange={this.handleInputChange}>
+                                {options.map((option) => (
+                                <option value={option.value}>{option.label}</option>
+                                ))}
+                            </select>
+                            </Col>
+                        </Form.Group>
+                        {/* <Row>
+                            <Col sm="2" />
+                            <Col sm="4">
+                                <Button variant="success" type="submit" value="Submit">Simpan</Button> {' '}
+                                <Button variant="danger" onClick={this.props.onHide}>Batal</Button>
+                            </Col>
+                        </Row> */}
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" type="submit" value="Submit" onClick={this.handleSubmit}>Simpan</Button> {' '}
+                    <Button variant="danger" onClick={this.props.onHide}>Batal</Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
 }
+
+// function AddUsersModal(props) {
+//     return (
+//         <Modal
+//         {...props}
+//         size="lg"
+//         aria-labelledby="contained-modal-title-vcenter"
+//         centered
+//         >
+//         <Modal.Header closeButton>
+//             <Modal.Title id="contained-modal-title-vcenter">
+//             Tambah User
+//             </Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//         <Form>
+//                 <Form.Group as={Row} controlId="formGroupRole">
+//                     <Form.Label column sm="2">Role: </Form.Label>
+//                     <Col sm="10">
+//                         <Form.Control as="select" defaultValue="Choose...">
+//                             <option>Priority Customers (PCu)</option>
+//                             <option>Personal Banking Assistent (PBA)</option>
+//                             <option>Personal Banking Assistent Manager (PBAM) </option>
+//                             <option>Banking Premium Booking Administration (BPBA) </option>
+//                         </Form.Control>
+//                     </Col>
+//                 </Form.Group>
+//                 <Form.Group as={Row} controlId="formGroupName">
+//                     <Form.Label column sm="2">Nama: </Form.Label>
+//                     <Col sm="10">
+//                         <Form.Control type="text"/>
+//                     </Col>
+//                 </Form.Group>
+//                 <Form.Group as={Row} controlId="formGroupEmail">
+//                     <Form.Label column sm="2">Email 1: </Form.Label>
+//                     <Col sm="10">
+//                         <Form.Control type="email"/>
+//                     </Col>
+//                 </Form.Group>
+//                 <Form.Group as={Row} controlId="formGroupEmail2">
+//                     <Form.Label column sm="2">Email 2: </Form.Label>
+//                     <Col sm="10">
+//                         <Form.Control type="email"/>
+//                     </Col>
+//                 </Form.Group>
+//                 <Form.Group as={Row} controlId="formGroupStatus">
+//                     <Form.Label column sm="2">Status: </Form.Label>
+//                     <Col sm="3">
+//                         <Form.Control as="select" defaultValue="Choose...">
+//                             <option>Enable</option>
+//                             <option>Disable</option>
+//                         </Form.Control>
+//                     </Col>
+//                 </Form.Group>
+//             </Form>
+//         </Modal.Body>
+//         <Modal.Footer>
+//             <Button variant="success">Simpan</Button>
+//             <Button variant="danger" onClick={props.onHide}>Batal</Button>
+//         </Modal.Footer>
+//         </Modal>
+//     )
+// }
 
 export default function UsersPage() {
     const[modalAdd, setModalAdd] = useState(false);
